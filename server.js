06fe695a8439407a335ebec1ca3b804eb26ae454
Jeff McDonald <jwmcdonald85@gmail.com>
2023-12-20 20:12:28 -0500

    add ejs dependencies

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const morgan = require("morgan")
 const app = express();
 const { PORT = 3013 } = process.env;
 
-
+const Book = require("./models/Book.js")
 
 
 
@@ -26,18 +26,29 @@ app.use(express.urlencoded({ extended: true })) // body parser this is how we ge
 
 
 // NEW - GET for the form to create a new book
+app.get("/books/new", (req, res) => {
+    res.send("new book")
+})
 
 
 // Create - POST
 app.post("/books", async (req, res) => {
-    if (req.body.completed === "on") {
-        // if checked
-        req.body.completed = true
-    } else {
-        // if not checked
-        req.body.completed = false
+    
+    try {
+        if (req.body.completed === "on") {
+            // if checked
+            req.body.completed = true
+        } else {
+            // if not checked
+            req.body.completed = false
+        }
+        //res.send(req.body)
+        let newBook = await Book.create(req.body)
+        res.send(newBook)
+
+    } catch (err) {
+        res.send(err)
     }
-    res.send(req.body)
 })
 
 
